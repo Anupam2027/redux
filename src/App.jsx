ce1c32726e87a458f4bdca949b92fabea3e334ac
Anupam Kumari Mishra <131358927+Anupam2027@users.noreply.github.com>
2025-03-23 07:52:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AddTodo from "./component/AddTodo";
+import Todos from "./component/Todos";
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
